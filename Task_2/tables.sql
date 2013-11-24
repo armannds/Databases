1a37f6b87f984c59555f2c9354358ea23562a40f
armanns <armanns@applicon.is>
2013-11-24 21:52:21 +0100

    Created all tables

--- Task_2/tables.sql
+++ Task_2/tables.sql
@@ -32,9 +32,24 @@ CREATE TABLE Courses
 			ON UPDATE CASCADE
 );
 
+CREATE TABLE LimitedCourses
+(
+	course CHAR(6) REFERENCES Courses (code),
+	nrStudents INT,
+	PRIMARY KEY (code)
+);
+
 CREATE TABLE CourseTypes
 (
 	type VARCHAR(255) PRIMARY KEY
+);
+
+CREATE TABLE TypeOf
+(
+	course CHAR(6),
+	type VARCHAR(255),
+	FOREIGN KEY (course) REFERENCES Courses (code),
+	FOREIGN KEY (type) REFERENCES CourseTypes (type),
 );
 
 CREATE TABLE Students
@@ -52,4 +67,68 @@ CREATE TABLE HasProgrammes
 	programme VARCHAR(255),
 	FOREIGN KEY (department) REFERENCES Departments (name),
 	FOREIGN KEY (programme) REFERENCES Programmes (name),
-);  
+); 
+
+/*TODO add conditions for on update and on delete*/
+CREATE TABLE HasMandatory
+(
+	programme VARCHAR(255),
+	course CHAR(6),
+	FOREIGN KEY (programme) REFERENCES Programmes (name),
+	FOREIGN KEY (course) REFERENCES Courses (code),
+); 
+
+/*TODO add conditions for on update and on delete*/
+CREATE TABLE BrancMandatory
+(
+	branch VARCHAR(255),
+	programme VARCHAR(255),
+	course CHAR(6),
+	FOREIGN KEY (branch) REFERENCES Branches (name),
+	FOREIGN KEY (programme) REFERENCES Branches (programme),
+	FOREIGN KEY (course) REFERENCES Courses (code),
+);
+
+CREATE TABLE HasRecommended
+(
+	branch VARCHAR(255),
+	programme VARCHAR(255),
+	course CHAR(6),
+	FOREIGN KEY (branch) REFERENCES Branches (name),
+	FOREIGN KEY (programme) REFERENCES Branches (programme),
+	FOREIGN KEY (course) REFERENCES Courses (code),	
+);
+
+CREATE TABLE HasPrerequisites
+(
+	course CHAR(6),
+	prereqCourse CHAR(6),
+	FOREIGN KEY (course) REFERENCES Courses (code),
+	FOREIGN KEY (prereqCourse) REFERENCES Courses (code),
+);
+
+CREATE TABLE RegisteredTo
+(
+	student CHAR(10),
+	course CHAR(6),
+	FOREIGN KEY (student) REFERENCES Students (id),
+	FOREIGN KEY (course) REFERENCES Courses (code),		
+);
+
+CREATE TABLE Finished
+(
+	student CHAR(10),
+	course CHAR(6),
+	grade CHAR(1),
+	FOREIGN KEY (student) REFERENCES Students (id),
+	FOREIGN KEY (course) REFERENCES Courses (code),
+);
+
+CREATE TABLE InQueue
+(
+	student CHAR(10),
+	limitedCourse CHAR(6),
+	timeRegistered DATETIME,
+	FOREIGN KEY (student) REFERENCES Students (id),
+	FOREIGN KEY (course) REFERENCES LimitedCourses (code),
+);
