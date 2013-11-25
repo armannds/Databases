68f666f716a7fa4161c97c42a790fcebd3148218
armanns <armanns@applicon.is>
2013-11-25 17:48:25 +0100

    meh

--- Task_2/insert.sql
+++ Task_2/insert.sql
@@ -1,3 +1,10 @@
 INSERT INTO Departments 
 	VALUES ('Computer Science and Engineering', 'CSE'),
-		   ('Applied Mechanics', 'AMECH')
+		   ('Applied Mechanics', 'AMECH'),
+		   ('Signals and Systems', 'SS'),
+		   ('Product and Production Development', 'PPD');
+
+INSERT INTO Programme
+	VALUES ()
+
+
